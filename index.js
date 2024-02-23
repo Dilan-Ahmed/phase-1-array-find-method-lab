8dfe9f5fb64f6766607f805c86eea94c89800d7f
Dilan Ahmed <dilan98mustafa@gmail.com>
2024-02-23 19:53:57 +0300

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+const record = [
+  { year: "2015", result: "W" },
+  { year: "2014", result: "N/A" },
+  { year: "2013", result: "L" },
+  //...
+];
+
+function superbowlWin(array) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].result === "W") {
+      return array[i].year;
+    }
+  }
+  return undefined;
+}
+
+superbowlWin(record);
+console.log(superbowlWin(record));
